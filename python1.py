ce997aa140098a8bb12a4c36afa865902869b25c
kalyani <kalyanikandula279@gmail.com>
2024-07-04 11:59:43 +0530

    third

--- python1.py
+++ python1.py
@@ -1,2 +1,6 @@
-print('hello')
-print ("hi")
+def calculate_area_of_circle(radius=2):
+    area = math.pi*radius**2
+    print("radius:", radius)
+    print("area of circle =",area)
+    return area
+
